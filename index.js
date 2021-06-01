060d70c96cab9e1fd894c00756f744b9628176db
Shally <1672977@students.wits.ac.za>
2021-06-01 02:15:54 +0200

    Updated menu

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 const navbarMenuToggler = document.querySelector(".navbarMenu-toggler");
 const navbarMenu = document.querySelector(".navbarMenu ul");
 const navbarMenuLinks = document.querySelectorAll(".navbarMenu a")
 navbarMenuToggler.addEventListener("click", navbarMenuTogglerClick);
+
 
 function navbarMenuTogglerClick()
 {
@@ -10,9 +12,36 @@ function navbarMenuTogglerClick()
 
 }
 navbarMenuLinks.forEach (elem => elem.addEventListener("click", navbarMenuLinkClick))
+
 function navbarMenuLinkClick()
 {
     if (navbarMenu.classList.contains("open")){
         navbarMenuToggler.click();
+
     }
+
+}
+
+document.getElementById("blogs").addEventListener("click", openMenu);
+function openMenu(){
+
+document.getElementById("dropdown").classList.toggle("active");
+
+}
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbarMenu");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
