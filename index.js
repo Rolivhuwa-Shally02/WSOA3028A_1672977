838ac8e4ce99bd5167612a7cad217c4c489bfe35
Shally <1672977@students.wits.ac.za>
2021-07-05 06:45:04 +0200

    blogs html

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ toggleNav.addEventListener("click", function () {
   toggleNav.classList.toggle("open");
 })
 
-/*window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()};
 
 // Get the navbar
 let navbar = document.getElementById("desktop-nav");
@@ -74,88 +74,39 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const navbarMenuToggler = document.querySelector(".navbarMenu-toggler");
-
-
-const navbarMenu = document.querySelector(".navbarMenu ul");
-const navbarMenuLinks = document.querySelectorAll(".navbarMenu a")
-navbarMenuToggler.addEventListener("click", navbarMenuTogglerClick);
-
-
-function navbarMenuTogglerClick()
-{
-    navbarMenuToggler.classList.toggle("open-navbarMenu-toggler");
-    navbarMenu.classList.toggle("open")
-
-}
-navbarMenuLinks.forEach (elem => elem.addEventListener("click", navbarMenuLinkClick))
-
-function navbarMenuLinkClick()
-{
-    if (navbarMenu.classList.contains("open")){
-        navbarMenuToggler.click();
-
-    }
-
 }
 
-document.getElementById("blogs").addEventListener("click", openMenu);
-function openMenu(){
 
-document.getElementById("dropdown").classList.toggle("active");
 
-}
 
-window.onscroll = function() {myFunction()};
 
-// Get the navbar
-let navbar = document.getElementById("navbarMenu");
 
-// Get the offset position of the navbar
-let sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
